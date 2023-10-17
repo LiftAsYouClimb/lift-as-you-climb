34f9596644c8621f38d9b1e94c849329754a56c6
“Lauren <lucerolaurenm@gmail.com>
2023-10-17 18:52:15 -0400

    Create the Passage Middleware and apply to protected routes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
-import express from "express";
-// create an instance of the Express application
+const express = require("express");
 const app = express();
+const passageAuthMiddleware = require("./authMiddleware"); // Import the middleware
+
 // define the port
 const port = process.env.PORT || 4000;
 // set up a route for HTTP GET requests to the root URL ("/")
 app.get("/", (req, res) => {
   res.send("Hello from the Node.js server!");
+});
+
+app.get("/authenticatedRoute", passageAuthMiddleware, async (req, res) => {
+  // Access the userID variable from the res object
+  let userID = res.userID;
+
+  // Now, we can use the userID in your route logic
+  // For example, we might use it to fetch user-specific data or perform actions on behalf of the authenticated user
 });
 
 app.listen(port, () => {
