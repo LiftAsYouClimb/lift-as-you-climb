339f557670d321e6e76d83456882f4a393289e2e
Ana Bennett <anabennett1111@gmail.com>
2023-10-17 12:23:02 -0500

    Implemented MUI navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./NavBar";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <NavBar />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
