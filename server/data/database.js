fdc49aa79bf3fe0c090c6555d7a41950ccf7fac3
“Lauren <lucerolaurenm@gmail.com>
2023-10-18 18:35:43 -0400

    Define database schema
    - The Users table stores user profiles
    - The SocialMediaLinks table allows users to have multiple social media links associated with their profiles
    - The Requests table stores user requests and links them to specific users
    - The Responses table stores responses to requests and links them to both the request and the user who responded

--- server/data/database.js
+++ server/data/database.js
@@ -9,4 +9,57 @@ const db = new sqlite3.Database(dbFilePath, (err) => {
   }
 });
 
+db.serialize(() => {
+  // Create the Users table
+  db.run(`
+    CREATE TABLE IF NOT EXISTS Users (
+      id INTEGER PRIMARY KEY,
+      email TEXT NOT NULL,
+      userName TEXT,
+      bio TEXT,
+      professionalBackground TEXT,
+      location TEXT
+    )
+  `);
+
+  db.run(`
+    CREATE TABLE IF NOT EXISTS SocialMediaLinks (
+      id INTEGER PRIMARY KEY,
+      user_id INTEGER,
+      platform TEXT NOT NULL, -- e.g., "Twitter," "LinkedIn," "Instagram"
+      link TEXT NOT NULL,
+      FOREIGN KEY (user_id) REFERENCES Users (id)
+    )
+  `);
+
+  // Create the Encouragement Requests table
+  db.run(`
+    CREATE TABLE IF NOT EXISTS Requests (
+      id INTEGER PRIMARY KEY,
+      user_id INTEGER,
+      title TEXT NOT NULL,
+      description TEXT NOT NULL,
+      emojiResponses TEXT, -- You can store emoji responses as a string or in a separate table
+      created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY (user_id) REFERENCES Users (id)
+    )
+  `);
+
+  // Create the Encouragement Responses table
+  db.run(`
+    CREATE TABLE IF NOT EXISTS Responses (
+      id INTEGER PRIMARY KEY,
+      request_id INTEGER,
+      user_id INTEGER,
+      type TEXT NOT NULL,
+      words TEXT NOT NULL,
+      resources TEXT,
+      linkDescription TEXT,
+      created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY (request_id) REFERENCES Requests (id),
+      FOREIGN KEY (user_id) REFERENCES Users (id)
+    )
+  `);
+});
+
 export default db; // Export the database connection
