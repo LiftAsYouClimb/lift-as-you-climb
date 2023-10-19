5fb27aac0e027ec9fa06548fe23beeb9fd21da4c
“Lauren <lucerolaurenm@gmail.com>
2023-10-19 14:12:17 -0400

    Endpoints WIP

--- server/data/database.mjs
+++ server/data/database.mjs
@@ -1,4 +1,5 @@
-const sqlite3 = require("sqlite3").verbose();
+import sqlite3 from "sqlite3";
+
 const dbFilePath = process.env.DB_FILE_PATH || "database.sqlite";
 
 const db = new sqlite3.Database(dbFilePath, (err) => {
@@ -10,25 +11,14 @@ const db = new sqlite3.Database(dbFilePath, (err) => {
 });
 
 db.serialize(() => {
-  // Create the Users table
+  // Create the UserProfiles table
   db.run(`
-    CREATE TABLE IF NOT EXISTS Users (
+    CREATE TABLE IF NOT EXISTS UserProfiles (
       id INTEGER PRIMARY KEY,
-      email TEXT NOT NULL,
       userName TEXT,
       bio TEXT,
       professionalBackground TEXT,
       location TEXT
-    )
-  `);
-
-  db.run(`
-    CREATE TABLE IF NOT EXISTS SocialMediaLinks (
-      id INTEGER PRIMARY KEY,
-      user_id INTEGER,
-      platform TEXT NOT NULL, -- e.g., "Twitter," "LinkedIn," "Instagram"
-      link TEXT NOT NULL,
-      FOREIGN KEY (user_id) REFERENCES Users (id)
     )
   `);
 
@@ -41,7 +31,7 @@ db.serialize(() => {
       description TEXT NOT NULL,
       emojiResponses TEXT, -- You can store emoji responses as a string or in a separate table
       created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-      FOREIGN KEY (user_id) REFERENCES Users (id)
+      FOREIGN KEY (user_id) REFERENCES UserProfiles (id)
     )
   `);
 
@@ -57,9 +47,13 @@ db.serialize(() => {
       linkDescription TEXT,
       created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
       FOREIGN KEY (request_id) REFERENCES Requests (id),
-      FOREIGN KEY (user_id) REFERENCES Users (id)
+      FOREIGN KEY (user_id) REFERENCES UserProfiles (id)
     )
   `);
 });
 
-export default db; // Export the database connection
+export function run(query, params, callback) {
+  return db.run(query, params, callback);
+}
+
+export { db };
--- server/server.mjs
+++ server/server.mjs
@@ -1,13 +1,14 @@
 import express from "express";
+import dataAccessLayer from "./dataAccess.mjs";
 import passageAuthMiddleware from "./authMiddleware.mjs";
 import dotenv from "dotenv";
+import { db } from "./data/database.mjs";
 
 dotenv.config();
 
 const app = express();
-// define the port
 const port = process.env.PORT || 4000;
-const db = require("./data/database"); // we can use the db object for database operations
+
 // set up a route for HTTP GET requests to the root URL ("/")
 app.get("/", (req, res) => {
   res.send("Hello from the Node.js server!");
@@ -17,8 +18,49 @@ app.get("/authenticatedRoute", passageAuthMiddleware, async (req, res) => {
   // Access the userID variable from the res object
   let userID = res.userID;
 
-  // Now, we can use the userID in your route logic
+  // Now, we can use the userID in our route logic
   // For example, we might use it to fetch user-specific data or perform actions on behalf of the authenticated user
+});
+
+// Define a GET route to retrieve user profiles
+app.get("/user-profiles", (req, res) => {
+  dataAccessLayer
+    .getUserProfiles()
+    .then((userProfiles) => {
+      // Send the user profiles as a JSON response
+      res.json(userProfiles);
+    })
+    .catch((error) => {
+      // Handle any errors (e.g., database errors)
+      console.error("Error fetching user profiles:", error);
+      res.status(500).json({ error: "Failed to fetch user profiles" });
+    });
+});
+
+// Define your API endpoints using Express.js routes
+app.use(express.json()); // Enable JSON parsing middleware
+
+app.post("/user-profiles", (req, res) => {
+  const { userName, bio, professionalBackground, location } = req.body;
+
+  const query = `
+    INSERT INTO UserProfiles (userName, bio, professionalBackground, location)
+    VALUES (?, ?, ?, ?)
+  `;
+
+  db.run(
+    query,
+    [userName, bio, professionalBackground, location],
+    function (err) {
+      if (err) {
+        console.error("Error creating user profile:", err.message);
+        res.status(500).json({ error: "Failed to create user profile" });
+      } else {
+        console.log("User profile created with ID:", this.lastID);
+        res.status(201).json({ message: "User profile created" });
+      }
+    }
+  );
 });
 
 app.listen(port, () => {
