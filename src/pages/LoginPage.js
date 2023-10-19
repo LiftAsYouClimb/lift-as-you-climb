153720f20564854ce5dd55180b9ca826543a28f8
Janika Hortizuela <jhortizuela21@gmail.com>
2023-10-19 11:10:16 -0600

    Resolve merge conflict

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,18 @@
-import "@passageidentity/passage-elements/passage-auth";
+import '@passageidentity/passage-elements/passage-auth';
+import '../styles/loginpage.css';
+import person from '../assets/2.png';
+import wordbubble from '../assets/wordbubble.png';
 
 function Login() {
   return (
-    <div>
-      <passage-auth app-id="e0EYlcepCkHve0EiiXrntHDA"></passage-auth>
+    <div className='login'>
+      <img src={person} />
+      <div className='passage'>
+        <passage-auth
+          app-id='e0EYlcepCkHve0EiiXrntHDA'
+          styles={{ border: 'solid red 1px' }}
+        ></passage-auth>
+      </div>
     </div>
   );
 }
